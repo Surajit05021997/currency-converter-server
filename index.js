075e42bd1de4b68cbe04899132b1333746f97252
Surajit Maity <surajitmaity12345@gmail.com>
2023-05-07 19:12:26 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const axios = require('axios');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = 9000;
+const app = express();
+
+const uri = `mongodb+srv://surajitmaity12345:${process.env.MONGODB_PASSWORD}@cluster0.gia9dyn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let currencyRateObj;
+async function run() {
+  setInterval(async () => {
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB");
+      const dbName = "currency_converter";
+      const collectionName = "currency_rate";
+      const database = client.db(dbName);
+      const collection = database.collection(collectionName);
+      
+      //get current rate from api
+      const response = await axios({
+        url: `https://api.api-ninjas.com/v1/convertcurrency?have=USD&want=INR&amount=1`,
+        headers: {
+          'X-Api-Key': process.env.API_KEY,
+        },
+      });
+      const currentRate = response.data.new_amount;
+      
+      currencyRateObj = await collection.findOne({ type: "USD_INR" });
+      if (currencyRateObj.rateList.length >= 5) {
+        currencyRateObj.rateList.shift();
+      }
+      currencyRateObj.rateList.push(currentRate);
+      await collection.updateOne(
+        { type: 'USD_INR' },
+        {
+          $set: { 'rateList': currencyRateObj.rateList, 'date': new Date() },
+          $currentDate: { lastModified: true }
+        }
+      );
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+      console.log("Disconnected");
+    }
+  }, 10000);
+}
+run().catch(console.dir);
+
+app.use('/', async (req, res) => {
+  res.json(currencyRateObj);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
