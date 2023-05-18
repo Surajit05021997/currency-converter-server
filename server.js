b522aee25d364cd9e29603869e197bb32d53b0e8
Surajit Maity <surajitmaity12345@gmail.com>
2023-05-18 22:54:41 +0530

    Added end point to update DB

--- server.js
+++ server.js
@@ -20,48 +20,6 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  setInterval(async () => {
-    try {
-      await client.connect();
-      const dbName = "currency_converter";
-      const collectionName = "currency_rate";
-      const database = client.db(dbName);
-      const collection = database.collection(collectionName);
-      
-      //get current rate from api
-      const response = await axios({
-        url: `https://api.api-ninjas.com/v1/convertcurrency?have=USD&want=INR&amount=1`,
-        headers: {
-          'X-Api-Key': process.env.API_KEY,
-        },
-      });
-      const currentRate = response.data.new_amount;
-      
-      const currencyRateObj = await collection.findOne({ type: "USD_INR" });
-      if (currencyRateObj.rateList.length >= 5) {
-        currencyRateObj.rateList.shift();
-      }
-      currencyRateObj.rateList.push({
-        'rate': currentRate,
-        'date': getCurrentDate(),
-      });
-      await collection.updateOne(
-        { type: 'USD_INR' },
-        {
-          $set: { 'rateList': currencyRateObj.rateList },
-          $currentDate: { lastModified: true }
-        }
-        );
-        console.log(`DB updated on ${new Date()}`);
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }, 1000 * 60 * 60 * 2);
-}
-run().catch(console.dir);
-
 function getCurrentDate() {
   const currentDate = new Date();
   const date = currentDate.getDate();
@@ -85,6 +43,47 @@ app.use('/api', async (req, res) => {
   } finally {
     await client.close();
     console.log("Disconnected");
+  }
+});
+
+app.use('/api/updateDB', async (req, res) => {
+  try {
+    await client.connect();
+    const dbName = "currency_converter";
+    const collectionName = "currency_rate";
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    
+    //get current rate from api
+    const response = await axios({
+      url: `https://api.api-ninjas.com/v1/convertcurrency?have=USD&want=INR&amount=1`,
+      headers: {
+        'X-Api-Key': process.env.API_KEY,
+      },
+    });
+    const currentRate = response.data.new_amount;
+    
+    const currencyRateObj = await collection.findOne({ type: "USD_INR" });
+    if (currencyRateObj.rateList.length >= 5) {
+      currencyRateObj.rateList.shift();
+    }
+    currencyRateObj.rateList.push({
+      'rate': currentRate,
+      'date': getCurrentDate(),
+    });
+    await collection.updateOne(
+      { type: 'USD_INR' },
+      {
+        $set: { 'rateList': currencyRateObj.rateList },
+        $currentDate: { lastModified: true }
+      }
+      );
+      res.send(`DB update successful. Updated on ${new Date()}.`);
+  } catch(error) {
+    console.log(error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 });
 
